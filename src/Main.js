783758bcb75880796a9f7df0457ea12665b4ccf1
Serg <serg27makar@gmail.com>
2024-10-20 18:57:51 +0300

    Init project.

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, {useState} from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import useToken from './components/hooks/useToken';
+import Home from "./pages/Home";
+import {Routes} from "react-router";
+import Registration from "./pages/Registration";
 
-export default function Main () {
+function Main() {
+
+    const { token, setToken } = useToken();
+    const [loginScreen, setLoginScreen ] = useState(true)
+
+    if(!token) {
+        if (loginScreen) return <Login setToken={setToken} changeScreen={setLoginScreen}/>
+        return <Registration setToken={setToken} changeScreen={setLoginScreen}/>
+    }
+
     return (
-        <div>Main</div>
-    )
+        <div className="wrapper">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
+
+export default Main;
